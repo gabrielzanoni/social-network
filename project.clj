66a1383ee70b5ddaa55ce42b11f78ccb1e980e84
Gabriel <gabriel.andreatto@gmail.com>
2016-03-15 19:03:22 -0300

    gitignore updated

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject nubank "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Nunbank Challenge: Social network"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-ring "0.8.11"]]
